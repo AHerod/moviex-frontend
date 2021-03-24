d688f7e08ecf14d3dc6897d74d3ec40837373010
AHerod <agn.j.herod@gmail.com>
2021-03-24 21:26:33 +0100

    add & setup nuxt-auth module, render action buttons in navbar based on authentication

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
   <v-app>
     <v-navigation-drawer
-      v-model="drawer"
+      v-model='drawer'
       app
     >
       <v-img src=''></v-img>
       <v-list>
         <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
+          v-for='(item, i) in items'
+          :key='i'
+          :to='item.to'
           router
           exact
         >
@@ -17,18 +17,38 @@
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title v-text='item.title' />
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="true"
+      :clipped-left='true'
       fixed
       flat
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-app-bar-nav-icon @click.stop='drawer = !drawer' />
+      <div v-if='isAuthenticated' class='flex w-full justify-end items-center'>
+        <a href="/profile">
+          <v-avatar>
+            <img
+              src='https://cdn.vuetifyjs.com/images/john.jpg'
+              alt='John'
+            >
+          </v-avatar>
+          <span class='text-black'>{{ loggedInUser.username }}</span>
+        </a>
+        <button @click='logout'>Logout</button>
+      </div>
+      <div>
+        <nuxt-link class="text-gray-700" to="/register">
+          <strong>Register</strong>
+        </nuxt-link>
+        <nuxt-link class="bg-yellow-500" to="/login">
+          Log in
+        </nuxt-link>
+      </div>
     </v-app-bar>
     <v-main>
       <v-container fluid>
@@ -36,7 +56,7 @@
       </v-container>
     </v-main>
     <v-footer
-      :absolute="false"
+      :absolute='false'
       app
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
@@ -45,8 +65,9 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
-  data () {
+  data() {
     return {
       drawer: false,
       items: [
@@ -64,6 +85,16 @@ export default {
       miniVariant: false,
       right: true,
       title: 'Moviex'
+    }
+  },
+  computed: {
+    ...mapGetters(['isAuthenticated', 'loggedInUser']),
+  },
+  mounted() {
+  },
+  methods: {
+    logout() {
+      console.log('LOGOUT')
     }
   }
 }
